0a3d2a33caeeead315db594a8d0666c4cfd04e62
Daniel Mangowi <daniel@.mangowicom>
2017-09-24 01:42:46 +0200

    custom filter for durations

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -7,7 +7,7 @@
         $scope.boolValue = true;
         $scope.mystyle = {color: "red"};
         $scope.myclass = "blue";
-        $scope.buttonDisable = true;
+        $scope.buttonDisable = false;
         $scope.sortOrder = "name";
          $scope.sortOrder = "-upVoteEvent";
 
@@ -26,7 +26,7 @@
                  name: "Intro to AngularJS",
                  creatorName: "Daniel Mangowi",
                  level: "Beginner",
-                 duration: '1 hour',
+                 duration: 1,
                  abstract: 'You will learn about angular how bla bla',
                  upVoteEvent: 0
              },
@@ -35,7 +35,7 @@
                     name: "Intro to AngularJS",
                     creatorName: "Daniel Mangowi",
                     level: "Beginner",
-                    duration: '1 hour',
+                    duration: 4,
                     abstract: 'You will learn about angular how bla bla',
                     upVoteEvent: 0
 
@@ -44,7 +44,7 @@
                     name: "Get work done",
                     creatorName: "Daniel Mangowi",
                     level: "Intermediate",
-                    duration: '45 hour',
+                    duration: 2,
                     abstract: 'We get into a project and we will learn about angular how bla bla',
                     upVoteEvent: 0
 
@@ -53,7 +53,7 @@
                     name: "Directives Master",
                     creatorName: "Daniel Mangowi",
                     level: "Advanced",
-                    duration: '1 hour',
+                    duration: 1,
                     abstract: 'You will learn about angular how bla bla',
                     upVoteEvent: 0
 
@@ -61,8 +61,8 @@
                 {
                     name: "AngularJS or React",
                     creatorName: "Daniel Mangowi",
-                    level: "ALL",
-                    duration: '1 hour',
+                    level: "Advanced",
+                    duration: 4,
                     abstract: 'We wil explore more into history',
                     upVoteEvent: 0
 
@@ -71,7 +71,7 @@
                     name: "Scope is always there",
                     creatorName: "Daniel Mangowi",
                     level: "Advanced",
-                    duration: '1 hour',
+                    duration: 12,
                     abstract: 'We will look  on how scope work and see what you can do with',
                     upVoteEvent: 0
 
@@ -80,7 +80,7 @@
                     name: "YGNT",
                     creatorName: "Daniel Mangowi",
                     level: "Beginner",
-                    duration: '12 hour',
+                    duration: 90,
                     abstract: 'You are not going to need it',
                     upVoteEvent: 0
                 }
