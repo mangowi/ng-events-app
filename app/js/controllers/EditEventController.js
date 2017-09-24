8faa8ba7df302a31ab233a2a0b21cd15bc7ed868
Daniel Mangowi <daniel@.mangowicom>
2017-09-24 02:43:37 +0200

    adding validation with ng-pattern and use ng-invalid ng-dirty to add custom color on validation input

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,8 +2,11 @@
 
 eventsApp.controller("EditEventController",
     function EditEventController($scope) {
-        $scope.saveEvent = function (data) {
-            window.alert("event " + data.name + " saved");
+        $scope.saveEvent = function (event, newEventForm) {
+            if(newEventForm.$valid){
+                window.alert("event " + event.name + " saved");
+
+            }
 
         };
 
